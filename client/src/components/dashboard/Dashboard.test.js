3fa49dfdc205ad4f95585f16bfe5f6f58e9f4d76
Vladimir Oluic <v.oluic@levi9.com>
2020-02-04 12:01:09 +0100

    FIRST TEST DONE

--- client/src/components/dashboard/Dashboard.test.js
+++
@@ -1,3 +0,0 @@
-describe('<Dashboard />', () => {
-	it('should render profile if authenticated');
-});
---
+++ client/src/components/layout/Spinner.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Spinner from './Spinner';
+
+configure({ adapter: new Adapter() });
+
+describe('<Spinner />', () => {
+	it('should render spinner gif', () => {
+		const wrapper = shallow(<Spinner />);
+		expect(wrapper.find('img'));
+	});
+});
